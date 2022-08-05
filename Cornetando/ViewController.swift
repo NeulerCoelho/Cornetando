0b5575afc89e37b0d23c3af03b0be50abac231c2
NeulerCoelho <neuler@gmail.com>
2022-08-04 22:47:43 -0300

    Initial Commit

---
+++ Cornetando/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cornetando
+//
+//  Created by Neuler Pereira on 04/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
